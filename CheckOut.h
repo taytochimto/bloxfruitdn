5268e3c21dcd62225ce24312d2ccf3453acde578
cupr0qwe <mhq18042007@gmail.com>
2026-07-10 09:29:16 +0700

    Create CheckOut.h

---
+++ CheckOut.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Common.h"
+
+void checkOut(vector<Phong>& dsPhong);
+void ghiFile(const vector<Phong>& dsPhong, const string& filename);
+void docFile(vector<Phong>& dsPhong, const string& filename);
