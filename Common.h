5e892c87caae0d141307c2fadb55319c96f39790
dinmp5 <vothanhtien2208@gmail.com>
2026-07-12 13:32:24 +0700

    Update Common.h

--- Common.h
+++ Common.h
@@ -1,4 +1,4 @@
-#pragma once // Chống lỗi khai báo trùng lặp thư viện
+#pragma once 
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,14 +10,14 @@
 
 using namespace std;
 
-// Bảng màu giao diện
+
 const wstring RESET   = L"\033[0m";
 const wstring RED     = L"\033[31m";
 const wstring GREEN   = L"\033[32m";
 const wstring YELLOW  = L"\033[33m";
 const wstring CYAN    = L"\033[36m";
 
-// Cấu trúc dữ liệu
+
 struct KhachHang {
     wstring ten = L"";
     wstring cccd = L"";
@@ -26,7 +26,7 @@ struct KhachHang {
 
 struct Phong {
     int soPhong;
-    wstring loaiPhong; // VIP, Đơn, Đôi
+    wstring loaiPhong; 
     double giaPhong;
     bool daDat = false;
     KhachHang khach;
