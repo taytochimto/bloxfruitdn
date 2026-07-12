d7548fbe132207558ebb8beee4e402b263e35281
dinmp5 <vothanhtien2208@gmail.com>
2026-07-12 13:35:15 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "DichVu.h"
 #include "CheckOut.h"
 
-// Tạo danh sách phòng mặc định nếu mở app lần đầu
+
 void khoiTaoPhongMacDinh(vector<Phong>& dsPhong) {
     dsPhong.push_back({101, L"Đơn", 200000});
     dsPhong.push_back({102, L"Đơn", 200000});
@@ -14,13 +14,13 @@ void khoiTaoPhongMacDinh(vector<Phong>& dsPhong) {
 }
 
 int main() {
-    // Ép Console sang tiếng Việt
+    
     _setmode(_fileno(stdout), _O_U16TEXT);
     _setmode(_fileno(stdin), _O_U16TEXT);
 
     vector<Phong> dsPhong;
     
-    // Nạp dữ liệu cũ (Người 4)
+    
     docFile(dsPhong, "data.txt");
     if (dsPhong.empty()) {
         khoiTaoPhongMacDinh(dsPhong);
@@ -28,16 +28,16 @@ int main() {
 
     int luaChon;
     do {
-        hienThiMenu(); // (Người 1)
-        luaChon = nhapSoNguyenSafe(); // (Người 3)
+        hienThiMenu(); 
+        luaChon = nhapSoNguyenSafe(); 
 
         switch (luaChon) {
-            case 1: hienThiSoDoPhong(dsPhong); break;         // (Người 1)
-            case 2: checkIn(dsPhong); break;                  // (Người 3)
-            case 3: goiDichVu(dsPhong); break;                // (Người 2)
-            case 4: timKiemKhachHang(dsPhong); break;         // (Người 2)
-            case 5: checkOut(dsPhong); break;                 // (Người 4)
-            case 6: ghiFile(dsPhong, "data.txt"); break;      // (Người 4)
+            case 1: hienThiSoDoPhong(dsPhong); break;         
+            case 2: checkIn(dsPhong); break;                  
+            case 3: goiDichVu(dsPhong); break;                
+            case 4: timKiemKhachHang(dsPhong); break;         
+            case 5: checkOut(dsPhong); break;                 
+            case 6: ghiFile(dsPhong, "data.txt"); break;      
             case 0:
                 ghiFile(dsPhong, "data.txt");
                 wcout << L"Cảm ơn bạn đã sử dụng phần mềm!\n";
